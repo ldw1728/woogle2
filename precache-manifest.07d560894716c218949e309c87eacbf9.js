8999861aad77e213feb1534652427a93ba047572
ldw1728 <zckxw@naver.com>
2020-12-14 14:39:36 +0900

    Updates

---
+++ precache-manifest.07d560894716c218949e309c87eacbf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11423b2cbf4afb353866ed6e237371b",
+    "url": "/woogle2/index.html"
+  },
+  {
+    "revision": "7901bb7fab90d8d532c4",
+    "url": "/woogle2/static/css/main.20984921.chunk.css"
+  },
+  {
+    "revision": "2e2b9ff1ba4d870c6353",
+    "url": "/woogle2/static/js/2.b560c0a0.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/woogle2/static/js/2.b560c0a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7901bb7fab90d8d532c4",
+    "url": "/woogle2/static/js/main.ba655434.chunk.js"
+  },
+  {
+    "revision": "59bad095ca8671653517",
+    "url": "/woogle2/static/js/runtime-main.fc07341d.js"
+  }
+]);
